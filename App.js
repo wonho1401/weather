d76eae6ba840975150ff61ea25dc1acc1aeba367
정원호 <wonho1401@naver.com>
2020-08-10 11:01:15 +0900

    Add Weather condition and Icon

--- App.js
+++ App.js
@@ -2,11 +2,49 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import { Alert } from "react-native";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "65f3521f68b7bd41f894b262c4957207";
 
 export default class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    //Send to API and get Weather!
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    //백틱 -> 변수를 문자열에 포함시킬 때 사용.
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
+  };
+
   getLocation = async () => {
-    const location = Location.getCurrentPositionAsync(); //parameter option -> permission 해야함. 위치 받아오는 것.
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync(); //parameter option -> permission 해야함. 위치 받아오는 것.
+
+      console.log(latitude, longitude);
+
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Cannot find you", "So Sad..");
+    }
   };
 
   componentDidMount() {
@@ -14,7 +52,12 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <Loading />;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
 
---
+++ Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+export default function Weather({ temp }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons size={80} name="weather-lightning-rainy" />
+        <Text style={styles.temp}>{temp} ᵒｃ</Text>
+      </View>
+      <View style={styles.halfContainer} />
+    </View>
+  );
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+  ]).isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 36,
+  },
+});
